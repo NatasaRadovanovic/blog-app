56625d5aad13a0a0c4ca0eb106f4745485de132d
NatasaRadovanovic <natasar.radovanovic@gmail.com>
2018-08-17 21:46:32 +0200

    fixed-bug

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -5,21 +5,21 @@
     <form class="formWrapp" @submit.prevent="onSubmit" @reset='reset' >
         <div class="form-group">
             <label for="title">Title</label>
-            <input v-validate="'required|min:2'" autofocus name="title" type="text" class="form-control" id="title" aria-describedby="emailHelp" 
+            <input v-validate.initial="'required|min:2'" autofocus name="title" type="text" class="form-control" id="title" aria-describedby="emailHelp" 
             placeholder="Title" v-model="post.title">
             <div class="helo-block alert alert-danger"  v-show="errors.has('title')">
                 {{ errors.first('title') }}
             </div>
         </div>
         <div class="form-group">
-            <textarea  v-validate="'required|max:300'" name="text" rows="6" cols="50" class="form-control" id="text"
+            <textarea  v-validate.initial="'required|max:300'" name="text" rows="6" cols="50" class="form-control" id="text"
              placeholder="Write post..." v-model="post.text"></textarea>
              <div class="helo-block alert alert-danger"  v-show="errors.has('text')">
                 {{ errors.first('text') }}
             </div>
         </div>
        <button v-if="(this.$route.params.id)" class="btn btn-success" @click='editPost' >Edit</button>  
-        <button v-else class="btn btn-success" :disabled="this.errors.items.length !== 0">Submit</button>
+        <button v-else class="btn btn-success" :disabled="errors.items.length !== 0">Submit</button>
         <input class="btn btn-danger" type="reset" value="Reset" />
         </form>
     </div>
@@ -36,11 +36,11 @@ export default {
    
   methods:{
       onSubmit(){
-         
-        if(this.errors.items.length !== 0){
+          console.log(this.errors);
+         if(this.errors.items.length !== 0){
+             
             return;
        }
-       console.log(5);
           this.$emit('onSubmit')
       },
       reset(){
